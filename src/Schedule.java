3b2b254f3e3d540dbcd74e89aa9b9c67ae96a906
vlad <udovenko555@mail.ru>
2020-04-22 16:30:43 +0600

    Commit 2: Create method "massiveEquals"

--- src/Schedule.java
+++ src/Schedule.java
@@ -19,10 +19,14 @@ public class Schedule {
         new Schedule(new String[]{"20.04.2020", "11:35", "Конструирование ПО", "Убалехт"});
         String[] groupInfo = {university.getNames()[0],faculty.getNames()[1],
                 institute.getNames()[0],group.getNames()[0],course.getNames()[2]};
+
+        return massiveEquals(dataGroup, groupInfo);
+    }
+
+    private static String[] massiveEquals(String[] dataGroup, String[] groupInfo) {
         if (Arrays.equals(dataGroup, groupInfo)) {
             return schedule;
-        }
-        else {
+        } else {
             return new String[]{"нет", "нет", "нет", "нет"};
         }
     }
